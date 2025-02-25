eeaaff334ba7f29245e51cc581a470b56c29f845
sarah-frankel <frankels024@gmail.com>
2025-02-25 10:36:14 +0200

    connect to angular

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,34 +1,11 @@
-//using BL;
 
-//var builder = WebApplication.CreateBuilder(args);
-//builder.Services.AddSingleton<BLManager>();
-//// Add services to the container.
-
-//builder.Services.AddControllers();
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
-//app.MapControllers();
-
-//app.Run();
 using BL;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 builder.Services.AddSingleton<BLManager>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Add services to the container.
 builder.Services.AddCors(options =>
@@ -36,20 +13,9 @@ builder.Services.AddCors(options =>
     options.AddPolicy("CorsPolicy", builder => builder.WithOrigins("http://localhost:4200", "development site")
     .AllowAnyHeader().AllowAnyMethod());
 });
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-//builder.Services.AddCors(options =>
-//{
-//    options.AddPolicy("AllowAngular", builder =>
-//    {
-//        builder.WithOrigins("http://localhost:4200")
-//               .AllowAnyHeader()
-//               .AllowAnyMethod();
-//    });
-//});
+
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
 {
@@ -57,16 +23,15 @@ var app = builder.Build();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
+
+
 app.UseCors("CorsPolicy");
+app.UseAuthorization();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAuthorization();
 app.MapControllers();
-app.UseCors("AllowAngular");
+//app.UseCors("AllowAngular");
 app.Run();
-
-
 
 
